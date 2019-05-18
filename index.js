7ce07c57fe84fddcff3e8f95a4344a27229f141f
Samantha brake <sammy.brake@gmail.com>
2019-05-18 06:30:38 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,9 @@
+Scooter with year, color, and model properties
+Driver with name, age, and experience properties
+PickupLocation with address and city properties
+
+function Scooter(year, color, model) {
+  this.year = year;
+  this.color = color; 
+  this.model = model; 
+}
