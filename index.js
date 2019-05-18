a4aa65bad2b11e6fd1eae84bb8c953eb052bdba5
Samantha brake <sammy.brake@gmail.com>
2019-05-18 06:30:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ PickupLocation with address and city properties
 
 function Scooter(year, color, model) {
   this.year = year;
-  this.color = color; 
-  this.model = model; 
+  this.color = color;
+  this.model = model;
 }
