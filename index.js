0aec646e08960dd14d2e76c28627be151716c1fa
Samantha brake <sammy.brake@gmail.com>
2019-05-18 06:31:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ Scooter with year, color, and model properties
 Driver with name, age, and experience properties
 PickupLocation with address and city properties
 
+function Driver(name, age, experience) {
+  this.name = name; 
+  this.age = age; 
+  this.experience = experience;
+};
 function Scooter(year, color, model) {
   this.year = year;
   this.color = color;
   this.model = model;
-}
+};
+
+
