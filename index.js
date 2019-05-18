80833217f956dae6ac4f0d6b9caa31efcf3c502d
Samantha brake <sammy.brake@gmail.com>
2019-05-18 06:32:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+function PickupLocation(address, city) {
+  this.address = address;
+  this.city = city;
+}
+
+function Driver(name, age, experience) {
+  this.name = name;
+  this.age = age;
+  this.experience = experience;
+};
+function Scooter(year, color, model) {
+  this.year = year;
+  this.color = color;
+  this.model = model;
+};
